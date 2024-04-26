c0f7a5611ed5073f5e4964b2cfa8cf97adb1373c
rktj-roshi <r.tsuji.24.4.1334@4nonome.com>
2024-04-26 21:43:15 +0900

    Python入門の解答

--- pi.py
+++ pi.py
@@ -1,7 +1,19 @@
 text = """
-    How I want a drink, alcoholic of course, after the heavy chapters involving
-    quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
+
+How I want a drink, alcoholic of course, after the heavy 
+chapters involving 
+quantum mechanics. 
+All of thy geometry, Herr Planck, is fairly hard.
+
 """
 
 # TODO
 
+import re
+sp=[s for s in re.split('[ ,.\n]',text)if s]
+
+number=list(map(len,sp))
+
+result = ''.join(str(num) for num in number)
+
+print(result)
